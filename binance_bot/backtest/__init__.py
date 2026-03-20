2bd493ad37894c1fd7c72f464f6a3b4dd12cc197
solser123 <solser123@users.noreply.github.com>
2026-03-21 00:17:33 +0900

    Fix backtest package exports for research

--- binance_bot/backtest/__init__.py
+++ binance_bot/backtest/__init__.py
@@ -1,9 +1,12 @@
+from ..backtest_engine import BacktestResult, run_backtest_for_symbol
 from .engine import BacktestBatchResult, BacktestEngine
 from .report import build_backtest_report, export_backtest_csv, export_backtest_html
 
 __all__ = [
+    "BacktestResult",
     "BacktestBatchResult",
     "BacktestEngine",
+    "run_backtest_for_symbol",
     "build_backtest_report",
     "export_backtest_csv",
     "export_backtest_html",
